15c19c77eea50dac5d35c9fb6df992f3cc9d06be
Raraph84 <contact@raraph.fr>
2025-12-07 13:51:12 +0100

    Add some sleep to avoid dhclient ipv6 timing issue

--- up-fiber.sh
+++ up-fiber.sh
@@ -2,7 +2,7 @@
 
 /etc/dhcp/dhclient-orange-generator.sh
 
+sleep 2
 dhclient -4 -i $WAN_INTERFACE.832 -cf /etc/dhcp/dhclient-orange-v4.conf -df /var/lib/dhcp/dhclient-orange-v4.duid -lf /var/lib/dhcp/dhclient-orange-v4.lease -v
-sleep 1
+sleep 2
 dhclient -6 -P -D LL -i $WAN_INTERFACE.832 -cf /etc/dhcp/dhclient-orange-v6.conf -df /var/lib/dhcp/dhclient-orange-v6.duid -lf /var/lib/dhcp/dhclient-orange-v6.lease -v -e WAN_INTERFACE=$WAN_INTERFACE -e LAN_INTERFACE=$LAN_INTERFACE
-sleep 1
